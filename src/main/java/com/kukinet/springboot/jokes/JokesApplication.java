9e20cb176c268b73f04140c6dc9dfd3e699b4e44
chenchuk <chenchuk@gmail.com>
2019-07-15 11:08:35 +0300

    working web service - refactor packages, main method as root of all packages - add a view

--- src/main/java/com/kukinet/springboot/jokes/JokesApplication.java
+++ src/main/java/com/kukinet/springboot/jokes/JokesApplication.java
@@ -1,4 +1,4 @@
-package com.kukinet.springboot.jokes.services;
+package com.kukinet.springboot.jokes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
